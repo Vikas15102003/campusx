de98dca627b3b7638ef7a9c6963d6839cc328e2f
vikas15102003 <vm80781@gmail.com>
2026-06-04 01:45:04 +0530

    Day 1 - Operators practice

--- campusx.py
+++ campusx.py
@@ -12,3 +12,16 @@ print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
 print("Division:", a / b)
+
+# Program find the sum of a 3 digit number entered by the user 
+num = int(input("Enter 3 digit number"))
+
+a = num % 10
+num = num // 10
+
+b = num % 10
+num = num // 10
+
+c = num  % 10
+
+print(a + b + c)
