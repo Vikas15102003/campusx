587b5995f2c0ed78a98d8c487e11a8ac5ff56c46
vikas15102003 <vm80781@gmail.com>
2026-06-04 02:45:23 +0530

    Day 1 - Operators practice

--- campusx.py
+++ campusx.py
@@ -1,5 +1,5 @@
 # Day 1 - Basic Python
-name = "Vikas"
+'''name = "Vikas"
 age = 22
 
 print("My Name Is", name)
@@ -11,17 +11,55 @@ b = 5
 print("Addition:", a + b)
 print("Subtraction:", a - b)
 print("Multiplication:", a * b)
-print("Division:", a / b)
+print("Division:", a / b)'''
 
 # Program find the sum of a 3 digit number entered by the user 
-num = int(input("Enter 3 digit number"))
+'''num = int(input("Enter 3 digit number"))
 
 a = num % 10
 num = num // 10
-
 b = num % 10
 num = num // 10
 
 c = num  % 10
 
-print(a + b + c)
+print(a + b + c)'''
+
+# If - else program 
+# login program and indentation 
+# email - vm80781@gmail.com 
+# pass - Vikas@321
+
+'''user = str(input("Enter Your Email"))
+password = str(input(" Enter Your Password"))
+
+email = "vm80781@gmail.com"
+passw = "Vikas@321"
+
+if email == user and passw == password:
+    print("Wel- Come Vikas")
+else:
+    print(" Valid Email or Password ")'''
+
+
+# Next Topic Modules In Python
+# Module is predefine library its mean the multiple fiction store in one module 
+''' 1) Math
+    2_ keyword
+    3) random
+    4) datetine
+import math
+print(math.factorial(5))
+
+import keyword
+print(keyword.kwlist)
+
+import random
+print(random.randint(1,100))
+
+import datetime
+print(datetime.datetime.now())'''
+
+# Loops In Python 
+''' 1) While
+    2) for '''
