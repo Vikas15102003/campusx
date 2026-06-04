f839d2f28a865c776746f890f9c7ae3f67f9646d
vikas15102003 <vm80781@gmail.com>
2026-06-05 03:01:12 +0530

    Day 2 - If-else and Conditions practice

--- campusx_0.py
+++ campusx_0.py
@@ -3,14 +3,21 @@
 #  Level 1 — If/Else (Easy)
 # Q1. Number positive hai, negative hai ya zero hai — check karo.
 
-num = int(input("Enter Number and Check the number Positive or Nigative"))
+'''num = int(input("Enter Number and Check the number Positive or Nigative"))
 if num<0:
     print("Number is Negative")
 elif num>0:
     print("Number is Positive")
 else:
-    print("valid Number")
+    print("valid Number")'''
 
+# Q2. Ek number lo — even hai ya odd check karo.
+
+num = int(input("Enter Any Number"))
+if num%2==0:
+    print("Number is Even")
+else:
+    print("Number is odd")
 # Loops In Python 
 ''' 1) While
     2) for '''
